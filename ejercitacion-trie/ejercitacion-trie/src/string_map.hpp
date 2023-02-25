b620a94069795a03e2f15c234f110a2295c7f14e
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-25 20:39:19 -0300

    Implementacion de todas las funciones de clase DiccTrie

--- ejercitacion-trie/ejercitacion-trie/src/string_map.hpp
+++ ejercitacion-trie/ejercitacion-trie/src/string_map.hpp
@@ -10,11 +10,6 @@ string_map<T>::string_map(const string_map<T>& aCopiar) : string_map() {
 }
 
 template <typename T>
-vector<pair<string, T>> string_map<T>::dicc() {
-    return dicc2;
-}
-
-template <typename T>
 string_map<T>& string_map<T>::operator=(const string_map<T>& d) {
     /* COMPLETAR */
     destruir(raiz);
@@ -26,6 +21,17 @@ string_map<T>& string_map<T>::operator=(const string_map<T>& d) {
 }
 
 template <typename T>
+vector<pair<string, T>> string_map<T>::dicc() {
+    return dicc2;
+}
+
+template <typename T>
+string_map<T>::~string_map(){
+    /* COMPLETAR */
+    destruir(raiz);
+}
+
+template <typename T>
 void string_map<T>::destruir(Nodo* r) {
     if (r != nullptr){
         for(int i = 0; i < 256; i++){
@@ -34,12 +40,6 @@ void string_map<T>::destruir(Nodo* r) {
         }
         delete r;
     }
-}
-
-template <typename T>
-string_map<T>::~string_map(){
-    /* COMPLETAR */
-    destruir(raiz);
 }
 
 template <typename T>
@@ -60,7 +60,7 @@ void string_map<T>::insert(const pair<string, T>& a) {
                     Nodo* aux = new Nodo();
                     nodo_actual->siguientes[j] = aux;
                 }
-                /** Se continuamos con la insercion de la siguiente letra de la clave y coloca a j en 256 asi se puede pasar a la siguiente letra */
+                /** Se continua con la insercion de la siguiente letra de la clave y coloca a j en 256 asi se puede pasar a la siguiente letra */
                 nodo_actual = nodo_actual->siguientes[j];
                 j = 256;
             }
@@ -71,6 +71,7 @@ void string_map<T>::insert(const pair<string, T>& a) {
     nodo_actual->status = true;
     dicc2.push_back(a);
 }
+
 
 template <typename T>
 int string_map<T>::count(const string& key) const{
@@ -93,11 +94,15 @@ int string_map<T>::count(const string& key) const{
         }
     }
     /** Ya se recorrio toda la clave key. Ahora, se verifica si se encuentra en el Trie . */
-    if (nodo_actual != nullptr && nodo_actual->status == true){
+    if (nodo_actual != nullptr){
+        if (nodo_actual->status == true){
             return 1;
         } else {
             return 0;
         }
+    } else {
+        return 0;
+    }
 }
 
 template <typename T>
@@ -109,8 +114,8 @@ const T& string_map<T>::at(const string& key) const {
         for(int j = 0; j < 256; j++) {
             if (j == int(key[i])) {
                 nodo_actual = nodo_actual->siguientes[j];
+                j = 256;
             }
-            j = 256;
         }
     }
     return nodo_actual->definicion;
@@ -131,26 +136,101 @@ T& string_map<T>::at(const string& key) {
     return nodo_actual->definicion;
 }
 
-
 template <typename T>
-T& string_map<T>::operator[](const string& clave){
+void string_map<T>::erase(const string& key) {
     /* COMPLETAR */
+    /** Se crea un diccionario auxiliar donde se van guardando todas las claves distintas a key (la que se quiere borrar) */
+    vector<pair<string,T>> diccaux;
+    for(int i = 0; i < dicc2.size(); i++) {
+        if(dicc2[i].first != key)
+            diccaux.push_back(dicc2[i]);
+    }
+    /** Ahora, diccDos vuelve a ser el diccionario original pero sin la clave a eliminar con su respectivo significado. */
+    dicc2 = diccaux;
+    Nodo* nodo_actual = raiz;
+    Nodo* nodo_aux = raiz;
+    int last = 0;
+    for(int i = 0; i < key.size();i++) {
+        for(int j = 0; j < 256; j++) {
+            if(hijos(nodo_actual) || nodo_actual->status || nodo_actual == raiz) {
+                nodo_aux = nodo_actual;
+                if (j == int(key[i])){
+                    last = j;
+                }
+            }
+            if (j == int(key[i])) {
+                nodo_actual = nodo_actual->siguientes[j];
+                j = 256;
+            }
+        }
+    }
+    _size--;
+    nodo_actual->status = false;
+    Nodo* nodo_borrar = nodo_aux->siguientes[last];
+    if (!hijo(nodo_actual)) {
+        destruir(nodo_borrar);
+        nodo_aux->siguientes[last] = nullptr;
+    }
 }
 
-
-
+template <typename T>
+bool string_map<T>::hijos(Nodo const* r) {
+    int acum = 0;
+    for (int i = 0; i < 256; i++) {
+        if (r->siguientes[i] != nullptr){
+            acum++;
+        }
+    }
+    if (acum > 1){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 template <typename T>
-void string_map<T>::erase(const string& clave) {
-    /* COMPLETAR */
+bool string_map<T>::hijo(Nodo const* r) {
+    for (int i = 0; i < 256; i++) {
+        if (r->siguientes[i] != nullptr){
+            return true;
+        }
+    }
+    return false;
 }
 
 template <typename T>
 int string_map<T>::size() const{
     /* COMPLETAR */
+    return _size;
+
 }
 
 template <typename T>
 bool string_map<T>::empty() const{
     /* COMPLETAR */
+    return _size == 0;
 }
+
+template <typename T>
+T& string_map<T>::operator[](const string& key){
+    Nodo* nodo_actual = raiz;
+    for(int i = 0; i < key.size();i++) {
+        for(int j = 0; j < 256; j++) {
+            if (j == int(key[i])) {
+                nodo_actual = nodo_actual->siguientes[j];
+                j = 256;
+            }
+        }
+    }
+    return nodo_actual->definicion;
+}
+
+
+
+
+
+
+
+
+
+
